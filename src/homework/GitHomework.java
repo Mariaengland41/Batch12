efcfdd6af7d30285e59ff4ce6b31bcbf12dbd6f5
maria <maria@10.10.111.184>
2024-09-11 18:05:36 -0400

    Removed all code

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -2,18 +2,9 @@ package homework;
 
 public class GitHomework {
 	
-	public void display() {
-		System.out.println ("John Cena");
-	}
 	
-	/**
-          This is coming from GitHub
-	  Java is fun
-         Git is EASY
-         API is ot fun
-
-	*/
 	
+		
 		
 	}
 
